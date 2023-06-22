253bac7c99ec1178a1879d98c9fd27a4d8cb2009
Lil-Trill <danil.yurin.1990@mail.ru>
2023-06-22 17:07:33 +0300

    export implemented

--- api/controllers/controllerExport.php
+++ api/controllers/controllerExport.php
@@ -6,4 +6,21 @@ include $_SERVER['DOCUMENT_ROOT'].'/test/config.php';
 
 $obj = new Export();
 
+$id = $_POST['idProduct'];
+
+$data = $obj->connect->query("SELECT * FROM `product` WHERE `id`= $id");
+
+
+
+while($row = $data->fetch_assoc()){
+    $array = array(
+        array($row['id'],$row['name'],$row['name_trans'],$row['price'],$row['small_text'],$row['big_text'],$row['user_id'])
+    );
+    $obj->createCSVFile($array);
+}
+
+var_dump($array);
+
+$obj->createFile();
+
 echo $obj->string;
--- api/export.php
+++ api/export.php
@@ -1,5 +1,27 @@
 <?php
 class Export extends Core {
     public $string = "helloasedth";
+
+    //запись файла
+    public function createFile(){
+        $fp = fopen( $_SERVER['DOCUMENT_ROOT'].'/test/filesCSV/fl.txt','w');
+        fwrite($fp,'hello wrold');
+        fclose($fp); 
+    }
+
     
+
+
+     public function createCSVFile($array){
+        // $a = array(
+        //     array(1,2,3),
+        //     array('h1','hello','test'),
+        //     array(4,5,6)
+        // );
+        $file = fopen( $_SERVER['DOCUMENT_ROOT'].'/test/filesCSV/fileCSV'.$array[0][0].'.csv','w');
+        foreach($array as $line){
+            fputcsv($file,$line,';');
+        }
+        fclose($file);
+    }
 }
